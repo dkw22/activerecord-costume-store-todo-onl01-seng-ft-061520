1e0931d4117f8a3a12f2df284a85f7e7d9a10935
Devon Wehrlin <dkwehrlin.99@gmail.com>
2020-07-20 22:33:31 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,11 @@ ActiveRecord::Schema.define(version: 3) do
 
   create_table "costumes", force: :cascade do |t|
     t.string "name"
+    t.float "price"
+    t.string "size"
+    t.string "image_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "haunted_houses", force: :cascade do |t|
